e29d9f40a0f9c598483354e0775ab4c0337f8cf5
heheheheheh <kaijiec@outlook.com>
2017-04-29 10:10:40 -0700

    finsihed DejaVuMode class and its test

--- app/src/main/java/com/example/jeffphung/dejaphoto/DejaVuMode.java
+++ app/src/main/java/com/example/jeffphung/dejaphoto/DejaVuMode.java
@@ -6,56 +6,100 @@ package com.example.jeffphung.dejaphoto;
  * This is dejaVuMode class which contains all boolean values for modes inside the dejaVuMode
  * all modes are true by default
  *
- * use set+Modename(boolean b) to set turn on/off the mode
+ * call set+Modename(boolean b) to set turn on/off the mode
  *    - true is on, false is off
- * use is+Modename to get the current mode status
+ * call is+ModenameOn to get the current mode status
  *
  */
 
 public class DejaVuMode {
-    private boolean dejaVuMode = true;
-    private boolean locationMode = true;
-    private boolean timeMode = true;
-    private boolean dayMode = true;
+    private boolean dejaVuModeOn = true;
+    private boolean locationModeOn = true;
+    private boolean timeModeOn = true;
+    private boolean dayModeOn = true;
 
 
-    public boolean isDejaVuMode() {
-        return dejaVuMode;
+    /* return true if all sub-modes are off */
+    private boolean isAllModeOff(){
+        return !isDayModeOn()&&!isLocationModeOn()&&!isTimeModeOn();
     }
 
-    public void setDejaVuMode(boolean dejaVuMode) {
-        this.dejaVuMode = dejaVuMode;
-        //if user turn off dejaVuMode, all modes inside will automatically be turned off
-        if(!dejaVuMode){
-            setLocationMode(false);
-            setDayMode(false);
-            setTimeMode(false);
+    /* return true if all sub-modes are on */
+    private boolean isAllModeOn(){
+        return isDayModeOn()&&isLocationModeOn()&&isTimeModeOn();
+    }
+
+    public boolean isDejaVuModeOn() {
+        return dejaVuModeOn;
+    }
+
+    /* turn off DejaVuMode will automatically turn off all modes */
+    public void setDejaVuModeOn(boolean b) {
+        this.dejaVuModeOn = b;
+        if(!b){
+            this.dayModeOn=false;
+            this.locationModeOn=false;
+            this.timeModeOn=false;
+        }
+        else{
+            this.dayModeOn=true;
+            this.locationModeOn=true;
+            this.timeModeOn=true;
+        }
+
+    }
+
+    public boolean isLocationModeOn() {
+        return locationModeOn;
+    }
+
+
+    /* if received a true, it will turn on the DejaVuMode
+     * if all sub-modes are off, it will turn off the DejaVuMode
+     */
+    public void setLocationModeOn(boolean b) {
+        this.locationModeOn = b;
+        if(isAllModeOff()){
+            this.dejaVuModeOn = false;
+        }
+        else if(b){
+            this.dejaVuModeOn = true;
         }
     }
 
-    public boolean isLocationMode() {
-        return locationMode;
+    public boolean isTimeModeOn() {
+        return timeModeOn;
     }
 
-    public void setLocationMode(boolean locationMode) {
-        this.locationMode = locationMode;
+    /* if received a true, it will turn on the DejaVuMode
+     * if all sub-modes are off, it will turn off the DejaVuMode
+     */
+    public void setTimeModeOn(boolean b) {
+        this.timeModeOn = b;
+        if(isAllModeOff()){
+            this.dejaVuModeOn = false;
+        }
+        else if(b){
+            this.dejaVuModeOn = true;
+        }
     }
 
-    public boolean isTimeMode() {
-        return timeMode;
+    public boolean isDayModeOn() {
+        return dayModeOn;
     }
 
-    public void setTimeMode(boolean timeMode) {
-        this.timeMode = timeMode;
-    }
 
-    public boolean isDayMode() {
-        return dayMode;
+    /* if received a true, it will turn on the DejaVuMode
+     * if all sub-modes are off, it will turn off the DejaVuMode
+     */
+    public void setDayModeOn(boolean b) {
+        this.dayModeOn = b;
+        if(isAllModeOff()){
+            this.dejaVuModeOn = false;
+        }
+        else if(b){
+            this.dejaVuModeOn = true;
+        }
     }
-
-    public void setDayMode(boolean dayMode) {
-        this.dayMode = dayMode;
-    }
-
 
 }
