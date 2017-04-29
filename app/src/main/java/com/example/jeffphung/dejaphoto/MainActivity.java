5abf2c1183f270baac5ede9629278074a97b71f7
heheheheheh <kaijiec@outlook.com>
2017-04-29 10:12:32 -0700

    add dejaVuMode to mainActivity

--- app/src/main/java/com/example/jeffphung/dejaphoto/MainActivity.java
+++ app/src/main/java/com/example/jeffphung/dejaphoto/MainActivity.java
@@ -14,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // create initial dejaVuMode setting
+        DejaVuMode dejaVuMode = new DejaVuMode();
+
+
+
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
