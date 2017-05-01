28c4b7bfc48ab2205176c358ffc35393919a8bd4
Jeffrey <jjphung@ucsd.edu>
2017-04-30 23:15:15 -0700

    main display

--- app/src/main/java/com/example/jeffphung/dejaphoto/MainActivity.java
+++ app/src/main/java/com/example/jeffphung/dejaphoto/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.jeffphung.dejaphoto;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +25,48 @@ public class MainActivity extends AppCompatActivity {
         DejaVuMode dejaVuMode = new DejaVuMode();
 
 
-
-
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 
+        ToggleButton dButton = (ToggleButton) findViewById(R.id.dejaVuButton);
+        dButton.setOnCheckedChangeListener(this);
+        ToggleButton lButton = (ToggleButton) findViewById(R.id.locationButton);
+        lButton.setOnCheckedChangeListener(this);
+        ToggleButton timeDayButton = (ToggleButton) findViewById(R.id.timeDayButton);
+        timeDayButton.setOnCheckedChangeListener(this);
+        ToggleButton dayWeekButton = (ToggleButton) findViewById(R.id.dayWeekButton);
+        dayWeekButton.setOnCheckedChangeListener(this);
 
-        //TODO TEST ahhh!!
+
+        /*
+        //TODO figure out how to test this code (android sim doesn't know how to save camera pics to gallery)
+        //should get path to camera album photos
+        File cameraDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath());
+        File[] files = cameraDir.listFiles();
+
+
+        // should set the wallpaper to the first in our file array
+        File firstImage = null;
+
+        //if no images set a whatever image, else use first image.
+        if (files.length == 0){
+            //TODO set default
+        }
+        else{
+            firstImage = files[0];
+        }
+
+
+        Bitmap bitmap = BitmapFactory.decodeFile(firstImage.getAbsolutePath());
+        WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+        try {
+            myWallpaperManager.setBitmap(bitmap);
+            Toast.makeText(MainActivity.this, "Wallpaper set", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "Error setting wallpaper", Toast.LENGTH_SHORT).show();
+        }
+        */
+
     }
 
     @Override
@@ -59,4 +90,54 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        switch (buttonView.getId()){
+            case R.id.dejaVuButton:
+                if (isChecked) {
+                    // The toggle is enabled
+                    System.out.println("deja enabled!");
+                } else {
+                    // The toggle is disabled
+                    System.out.println("deja disabled!");
+                }
+                break;
+            case R.id.locationButton:
+                if (isChecked) {
+                    // The toggle is enabled
+                    System.out.println("location enabled!");
+                } else {
+                    // The toggle is disabled
+                    System.out.println("location disabled!");
+                }
+                break;
+            case R.id.timeDayButton:
+                if (isChecked) {
+                    // The toggle is enabled
+                    System.out.println("time of day enabled!");
+                } else {
+                    // The toggle is disabled
+                    System.out.println("time of day disabled!");
+                }
+                break;
+            case R.id.dayWeekButton:
+                if (isChecked) {
+                    // The toggle is enabled
+                    System.out.println("day of week enabled!");
+                } else {
+                    // The toggle is disabled
+                    System.out.println("day of week disabled!");
+                }
+                break;
+        }
+
+    }
+
+    public void createAlbum(View v){
+        System.out.println("Creating Custom Album");
+    }
+
 }
