2923eb7bedb1dcb0071cd3c33c2f4ed83f89c6dd
Romel Acosta <rjacosta@ucsd.edu>
2017-05-02 20:59:02 -0700

    Added baseline functionality for prev and next buttons

--- app/src/main/java/com/example/jeffphung/dejaphoto/NewAppWidget.java
+++ app/src/main/java/com/example/jeffphung/dejaphoto/NewAppWidget.java
@@ -1,15 +1,23 @@
 package com.example.jeffphung.dejaphoto;
 
+//import android.app.PendingIntent;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
  */
 
 public class NewAppWidget extends AppWidgetProvider {
+    private static  String rightButtonClicked = "rightButtonClicked";
+    private static  String leftButtonClicked = "leftButtonClicked";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -17,10 +25,25 @@ public class NewAppWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
+        // Intent for Right Button
+        Intent leftButtonIntent = new Intent(context, NewAppWidget.class);
+        leftButtonIntent.setAction(leftButtonClicked);
+        PendingIntent leftButtonPendingIntent = PendingIntent.getBroadcast(context, 0, leftButtonIntent, 0);
+
+        // Intent for Right Button
+        Intent rightButtonIntent = new Intent(context, NewAppWidget.class);
+        rightButtonIntent.setAction(rightButtonClicked);
+        PendingIntent rightButtonPendingIntent = PendingIntent.getBroadcast(context, 0, rightButtonIntent, 0);
+
+        views.setOnClickPendingIntent(R.id.buttonRight, rightButtonPendingIntent);
+        views.setOnClickPendingIntent(R.id.buttonLeft, leftButtonPendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
     }
 
     @Override
@@ -39,6 +62,19 @@ public class NewAppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equals(rightButtonClicked)) {
+            // put behavior here:
+            System.out.println("fghdgfhrsjkgfjhgjkrsjkghrjkgjkehfjkgaerhkjjhgjjjghvhjvhjvhjhjvjhgjhgkjgjhhfgh");
+        }
+        if (intent.getAction().equals(leftButtonClicked)) {
+            // put behavior here:
+            System.out.println("????????????????????????????????????????????????????????????????????????????????");
+        }
     }
 }
 
